a4615c4b1c04b267c80ed33cfbc9b3184cd3c3d9
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-17 19:53:59 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,3 +7,11 @@ start_date INTEGER,
 end_date INTEGER
 );
 
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category INTEGER,
+funding_goal TEXT,
+start_date INTEGER,
+end_date INTEGER
+);
