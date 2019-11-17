e1e69a8f944092c46a5b9edbbcf8ad9f27fa4b07
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-17 20:17:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,5 +33,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT users.name, users.age, SUM(pledges.amount) 
+  FROM users
+  JOIN pledges ON users.id = pledges.user_id 
+  GROUP BY users.name;"
 end
